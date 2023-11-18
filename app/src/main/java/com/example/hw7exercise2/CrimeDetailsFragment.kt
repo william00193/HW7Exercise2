2f661d1195fb5fd4bd095b72c68d60728ae1c8ea
texteditor <wh932S@login.missouristate.edu>
2023-11-18 12:21:50 -0600

    HW #7 Exercise #2 (11/18) Got it!

--- app/src/main/java/com/example/hw7exercise2/CrimeDetailsFragment.kt
+++ app/src/main/java/com/example/hw7exercise2/CrimeDetailsFragment.kt
@@ -2,20 +2,19 @@ package com.example.hw7exercise2
 
 import android.R
 import android.annotation.SuppressLint
-import android.app.ProgressDialog.show
-import android.content.DialogInterface
-import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils.isEmpty
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
-import android.window.OnBackInvokedDispatcher
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.addCallback
-import androidx.annotation.Nullable
-import androidx.core.text.trimmedLength
 import androidx.core.util.Preconditions.checkNotNull
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -26,8 +25,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.hw7exercise2.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
-import java.lang.Compiler.enable
 import java.util.*
+
+
 
 
 class CrimeDetailsFragment :Fragment() {
@@ -65,71 +65,59 @@ class CrimeDetailsFragment :Fragment() {
         return binding.root
 
 
+
     }
 
 
+        private fun setOnBackPressed() {
 
-private fun setOnBackPressed() {
+            requireActivity().onBackPressedDispatcher.addCallback(object :
+                OnBackPressedCallback(true) {
 
-    requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true){
+                    override fun handleOnBackPressed() {
 
 
+//Started with just the first variable, (crimeTitle == " ") wasn't working I tried a work around below
+//Everything worked when getText() was inserted between the title name and turining into string
 
-        override fun handleOnBackPressed() {
-
-            //Started with just the first variable, (crimeTitle == " ") wasn't working I tried a work around below
-            val crimeTitle = binding.crimeTitle.toString()
-            val codedCrimeTitleLength = crimeTitle.length
-
+                        val crimeTitle = binding.crimeTitle.getText().toString()
 
 //No matter how many characters I add to the title It continues to register as '130' in length and doesn't budge
-//What I left the condition to equal for debugging
-//Going back and fourth between ( != 130 ) & ( == 130 ) allows me to see a title being saved blank or not allowed
-            if (codedCrimeTitleLength == 130) {
+//Finally got this one, I had to change how I got the title with getText() and I referenced isEmpty() in the condition
 
-//Toast that will display the length of current crime title
-                Toast.makeText(binding.root.context,
-                    "$codedCrimeTitleLength",
-                    Toast.LENGTH_SHORT)
-                    .show()
+                            if (crimeTitle.isEmpty()) {
 
-//Toast that shows why (crimeTitle == " ") wont register
-                Toast.makeText(binding.root.context,
-                    "$crimeTitle",
-                    Toast.LENGTH_SHORT)
-                    .show()
 
 //Toast I will keep once debugging is done
-                Toast.makeText(binding.root.context,
-                    "Title Does Not Have a Value",
-                    Toast.LENGTH_SHORT)
-                    .show()
+                                Toast.makeText(
+                                    binding.root.context,
+                                    "Title Does Not Have a Value",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
 
-        } else  {
-
+                            } else {
 
 //What toast will be once debugging is complete
-                Toast.makeText(binding.root.context,
-                    "Title Has Been Saved",
-                    Toast.LENGTH_SHORT)
-                    .show()
+                                Toast.makeText(
+                                    binding.root.context,
+                                    "Title: '$crimeTitle' Has Been Saved",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
 
-//Toast that displays what the current title is registering as
-                Toast.makeText(binding.root.context,
-                    "$crimeTitle",
-                    Toast.LENGTH_SHORT)
-                    .show()
 
-                isEnabled = true
 
-                findNavController().popBackStack()
-        }
+                                isEnabled = true
 
-        }
+                                findNavController().popBackStack()
+                            }
 
-    })
+                        }
 
-}
+            })
+
+    }
 
 
 
